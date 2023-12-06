d5913dddacba90160e6933d39c0f3c024eed936e
nigelm48 <zcabnm0@ucl.ac.uk>
2023-12-06 19:09:34 +0000

    Third attempt at fixing links

--- app.js
+++ app.js
@@ -25,8 +25,51 @@ function collectNames() {
     nameInputsContainer.appendChild(submitButton);
 }
 
-// Include a UUID library, you can use a package like 'uuid' or generateUUID function
-// For simplicity, you can use a basic function as shown below
+function submitNames() {
+    let nameInputs = document.querySelectorAll("#nameInputs input");
+    let names = Array.from(nameInputs).map(input => input.value);
+
+    // Shuffle the array to randomize the pairing
+    let shuffledNames = shuffleArray(names.slice());
+
+    let nameInputsContainer = document.getElementById("nameInputs");
+    nameInputsContainer.innerHTML = ""; // Clear the existing content
+
+    shuffledNames.forEach((name, index) => {
+        // Find a name other than the current one (excluding themselves)
+        let recipient = findRecipient(name, shuffledNames);
+
+        // Generate unique links for each player
+        let link = generateUniqueLink(name, recipient);
+
+        // Create a paragraph element to display the player's name, recipient, and link
+        let playerInfo = document.createElement("p");
+        playerInfo.innerHTML = `<strong>${name}</strong> is the Secret Santa for: <strong>${recipient}</strong>. Link: <a href="${link}" target="_blank">${link}</a>`;
+
+        // Append the paragraph element to the container
+        nameInputsContainer.appendChild(playerInfo);
+    });
+}
+
+function findRecipient(currentName, names) {
+    // Filter out the current name
+    let potentialRecipients = names.filter(name => name !== currentName);
+
+    // Randomly choose a recipient from the remaining names
+    let recipient = potentialRecipients[Math.floor(Math.random() * potentialRecipients.length)];
+
+    return recipient;
+}
+
+// Function to generate a unique link based on the player's name and recipient's name
+function generateUniqueLink(playerName, recipientName) {
+    let uniqueIdentifier = generateUUID();
+    let relativePath = `${playerName.toLowerCase().replace(/\s/g, '-')}-${recipientName.toLowerCase().replace(/\s/g, '-')}-${uniqueIdentifier}`;
+    let netlifyDomain = 'https://gleeful-kulfi-bb3502.netlify.app/';
+    return `${netlifyDomain}${relativePath}`;
+}
+
+// Function to generate a UUID (Universally Unique Identifier)
 function generateUUID() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random() * 16 | 0,
@@ -35,42 +78,15 @@ function generateUUID() {
     });
 }
 
-function generateUniqueLink(playerName, phoneNumber) {
-    // Generate a UUID as a unique identifier
-    let uniqueIdentifier = generateUUID();
-    // Concatenate the player's name, phone number, and the unique identifier to create a unique link
-    let relativePath = `${playerName.toLowerCase().replace(/\s/g, '-')}-${phoneNumber}-${uniqueIdentifier}`;
-    // Use the Netlify domain as the base URL
-    let netlifyDomain = 'https://gleeful-kulfi-bb3502.netlify.app/';
-    return `${netlifyDomain}${relativePath}`;
+// Function to shuffle an array (Fisher-Yates algorithm)
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
-
-function submitNames() {
-    let nameInputs = document.querySelectorAll("#nameInputs input");
-    let names = Array.from(nameInputs).map(input => input.value);
-
-    let nameInputsContainer = document.getElementById("nameInputs");
-    nameInputsContainer.innerHTML = ""; // Clear the existing content
-
-    names.forEach((name, index) => {
-        // Generate a unique link for each player
-        let uniqueLink = generateUniqueLink(name);
-        
-        // Create a paragraph element to display the player's name and link
-        let playerInfo = document.createElement("p");
-        playerInfo.innerHTML = `<strong>${name}</strong>: <a href="#" data-target="${uniqueLink}" class="secret-link">${uniqueLink}</a>`;
-        
-        // Append the paragraph element to the container
-        nameInputsContainer.appendChild(playerInfo);
-    });
-
-    // Add event listener to all secret links
-    let secretLinks = document.querySelectorAll(".secret-link");
-    secretLinks.forEach(link => {
-        link.addEventListener("click", handleSecretLinkClick);
-    });
-}
 
 function handleSecretLinkClick(event) {
     event.preventDefault();
@@ -79,9 +95,9 @@ function handleSecretLinkClick(event) {
     let targetLink = event.target.dataset.target;
 
     // Extract player information from the link
-    let linkParts = targetLink.split('-');
-    let playerName = linkParts[0].replace(/%20/g, ' '); // Replace %20 with space
-    let phoneNumber = linkParts[1];
+    let linkParts = targetLink.split('/');
+    let playerName = decodeURIComponent(linkParts[1]); // Decode the player name
+    let phoneNumber = linkParts[2];
 
     // Display the Secret Santa recipient information on the page
     let recipientInfo = document.getElementById('recipientInfo');
